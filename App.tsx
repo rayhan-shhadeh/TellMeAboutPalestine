a0f51a5355f0cc76f48d4a0d57cce168c019eb8b
RayhanShadeh <re7an.ma7moud@gmail.com>
2025-12-27 19:25:17 +0200

    chore: update dependencies in package.json
    - downgraded expo version to ~54.0.0
    - updated react and react-dom to ^19.1.0
    - changed react-native-gesture-handler version to ~2.28.0
    - added react-native-web and react-native-worklets dependencies
    - updated @types/react-native and babel-preset-expo in devDependencies

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 /**
  * Tell Me About Palestine
  * Educational app for children aged 6-12
  * Main App entry point
  */
 
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native';
-import { LocalizationProvider } from './src/localization';
-import { RootNavigator } from './src/navigation/RootNavigator';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { LocalizationProvider } from "./src/localization";
+import { RootNavigator } from "./src/navigation/RootNavigator";
 
 export default function App() {
   return (
